0f5a28b083862946728369fa7cef73e19519b7c8
homecomp <0966009853m@gmail.com>
2020-04-16 23:00:49 +0300

    Hw3Updata_22:59_16.04.20

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,6 @@ import { HelloComponent } from './Component/hello/hello.component';
 import {HttpClientModule} from '@angular/common/http';
 import {UserResolverService} from './Servise/Resolvers/user-resolver.service';
 import {PostResolverService} from './Servise/Resolvers/post-resolver.service';
-import {CommentResolverService} from './Servise/Resolvers/comment-resolver.service';
 import { UserComponent } from './Component/single-componets/Users/user.component';
 import { PostComponent } from './Component/single-componets/post/post.component';
 import { CommentComponent } from './Component/single-componets/comment/comment.component';
@@ -21,16 +20,14 @@ const routes = [
   { path: '', component: HelloComponent},
   {
     path: 'users', component: AllUsersComponent , resolve: {list: UserResolverService}, children: [
-      {path: ':id/posts', component : AllPostComponent}
+      {path: ':id/posts', component : AllPostComponent},
     ]
   },
   { path: 'posts', component: AllPostComponent, resolve: {list: PostResolverService}, children: [
       {path: ':id/comments', component: AllCommentsComponent, resolve: {comments: CommentsResolverService}}
     ]
   },
-  { path: 'comments', component: AllCommentsComponent, resolve: {list: CommentsResolverService}, children: [
-      {path: ':id/post', component: PostComponent}
-    ]}
+  { path: 'comments', component: AllCommentsComponent, resolve: {list: CommentsResolverService}}
 ];
 
 @NgModule({
